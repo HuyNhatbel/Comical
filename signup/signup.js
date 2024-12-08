dfa8713f15fb747d213994c21fc45b2d2b2a5f45
Vu Huy Nhat <vuhuynhat001@gmail.com>
2024-12-08 10:07:10 +0700

    first commit

--- signup/signup.js
+++ signup/signup.js
@@ -5,6 +5,18 @@ document.getElementById("btnSignUp").addEventListener('click', function () {
     let password = document.getElementById("password").value;
     let retypepassword = document.getElementById("retypepassword").value;
 
+    if (username.length < 6) {
+        alert("Tên phải chứa tối đa 6 ký tự")
+    }
+
+    if (!email.includes("@")) {
+        alert("Sai định dạng email!");
+        return;
+    }
+    if (password.length < 6) {
+        alert("Mật khẩu phải chứa tối thiểu 6 kí tự!");
+        return;
+    }
     if (retypepassword == password) {
         if (!localStorage.getItem('users')) {
         localStorage.setItem('users', '[]');
@@ -23,7 +35,5 @@ document.getElementById("btnSignUp").addEventListener('click', function () {
     } else {
         alert("Mật khẩu không khớp!")
     }
-
-    
 });
 
