e7b50db4634c5c7415aaa9ae02ec0b6a3179a36a
Vu Huy Nhat <vuhuynhat001@gmail.com>
2025-04-13 08:23:53 +0700

    update

--- admin/manage_book/index.js
+++ admin/manage_book/index.js
@@ -1,5 +1,5 @@
 const userData = [
-    { title: "Năm Thứ Hai Sau Khi Kết Hôn", chapters: "86", image: "../../Images/nam-thu-hai.jpg", content: "Em có thể ly hôn với anh được không?” Hạ Vy bình tĩnh nói, đôi mắt trong trẻo không hề gợn sóng nhìn Trần Hạo.</h4> <h4>Trần Hạo sững sờ nhìn người phụ nữ trước mặt, không thể tin được những gì mình vừa nghe.</h4> <h4>Hạ Vy là vợ của anh suốt hai năm qua. Cô luôn dịu dàng, chăm sóc anh từng chút một, nhưng chưa bao giờ anh nghĩ rằng cô lại nói ra lời này.</h4> <h4>“Tại sao?” Trần Hạo nghẹn giọng hỏi, đôi mắt ánh lên sự hoảng loạn.</h4> <h4>Hạ Vy mỉm cười, nụ cười ấy dịu dàng nhưng lại mang theo một chút cay đắng: “Chúng ta không hợp nhau. Anh biết điều đó mà, đúng không?”</h4> <h4>Trần Hạo siết chặt bàn tay, sự đau đớn hiện rõ trên gương mặt: “Hạ Vy, em không thể rời đi. Anh cần em, anh không thể sống thiếu em…”</h4> <h4>Hạ Vy ngẩng đầu nhìn người đàn ông mà cô từng yêu sâu đậm, nhưng giờ đây tình cảm ấy chỉ còn là một mảnh ký ức.</h4> <h4>Cô bước tới, nhẹ nhàng đặt bàn tay lên vai anh: “Hạo, anh sẽ sống tốt mà. Em cũng vậy. Chúng ta hãy buông tay để bắt đầu lại từ đầu, được không?"
+    { title: "Năm Thứ Hai Sau Khi Kết Hôn", chapters: "86", image: "https://www.bgastore.ie/resource/u3NL/n5I/p4tYBuR12el/Posters/Bokstavs-posters/Poster_O.jpg", content: "Em có thể ly hôn với anh được không?” Hạ Vy bình tĩnh nói, đôi mắt trong trẻo không hề gợn sóng nhìn Trần Hạo.</h4> <h4>Trần Hạo sững sờ nhìn người phụ nữ trước mặt, không thể tin được những gì mình vừa nghe.</h4> <h4>Hạ Vy là vợ của anh suốt hai năm qua. Cô luôn dịu dàng, chăm sóc anh từng chút một, nhưng chưa bao giờ anh nghĩ rằng cô lại nói ra lời này.</h4> <h4>“Tại sao?” Trần Hạo nghẹn giọng hỏi, đôi mắt ánh lên sự hoảng loạn.</h4> <h4>Hạ Vy mỉm cười, nụ cười ấy dịu dàng nhưng lại mang theo một chút cay đắng: “Chúng ta không hợp nhau. Anh biết điều đó mà, đúng không?”</h4> <h4>Trần Hạo siết chặt bàn tay, sự đau đớn hiện rõ trên gương mặt: “Hạ Vy, em không thể rời đi. Anh cần em, anh không thể sống thiếu em…”</h4> <h4>Hạ Vy ngẩng đầu nhìn người đàn ông mà cô từng yêu sâu đậm, nhưng giờ đây tình cảm ấy chỉ còn là một mảnh ký ức.</h4> <h4>Cô bước tới, nhẹ nhàng đặt bàn tay lên vai anh: “Hạo, anh sẽ sống tốt mà. Em cũng vậy. Chúng ta hãy buông tay để bắt đầu lại từ đầu, được không?"
 }];
 
 // Khởi tạo DataTable
@@ -24,10 +24,14 @@ $(document).ready(function () {
             {
                 data: "image",
                 render: function (data) {
-                    return `<span class="me-2">📍</span>${data}`;
+                    return `<img src="${data}" class="img-container"/>`;
                 }
             },
-            { data: "content" },
+            { data: "content",
+                render: function (data) {
+                    return `<div class="content-text">${data}</div>`
+                }
+            },
             {
                 data: null,
                 render: function () {
